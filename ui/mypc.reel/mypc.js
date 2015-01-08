98a79da695f1ce5587409e3e4269a6a3c242ae9c
topierros <noreply>
2015-01-08 14:56:25 +0000

    Add component mypc

---
+++ ui/mypc.reel/mypc.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/mypc.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Mypc
+ * @extends Component
+ */
+exports.Mypc = Component.specialize(/** @lends Mypc# */ {
+    constructor: {
+        value: function Mypc() {
+            this.super();
+        }
+    }
+});
